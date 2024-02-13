df49211ec7a132b2b9dad204453cc95ce7654a8b
kunakorn topurin <97731297+Bright-Kunakorn@users.noreply.github.com>
2024-02-13 22:52:53 +0700

    Update LongestPalindrome.go

--- LongestPalindrome.go
+++ LongestPalindrome.go
@@ -1,49 +1,56 @@
-package main
-
-import (
-	"fmt"
-)
-func main() {
-	fmt.Println(longestPalindrome("baagg"))
-}
-
-func reverseString(s string) string {
-	bytes := []byte(s)
-	left := 0
-	right := len(bytes) - 1
-	for left < right {
-		bytes[left], bytes[right] = bytes[right], bytes[left]
-		left++
-		right--
+func preProcess(s string) string {
+	n := len(s)
+	if n == 0 {
+		return "^$"
 	}
-	return string(bytes)
+	
+	result := "^"
+	for i := 0; i < n; i++ {
+		result += "#" + string(s[i])
+	}
+	result += "#$"
+	return result
 }
 
 func longestPalindrome(s string) string {
-	word := ""
-	temps := ""
-	front := ""
-	back := ""
-	max := 0
+	T := preProcess(s)
+	n := len(T)
+	P := make([]int, n)
+	C, R := 0, 0
 
-	for i := 0; i < len(s)+1; i++ {
-		for j := 0; j < i; j++ {
-			temps = (s[j:i])
-			if len(temps)%2 == 0 {
-				front = temps[:len(temps)/2]
-				back = temps[len(temps)/2:]
-			} else {
-				front = temps[:len(temps)/2]
-				back = temps[len(temps)/2+1:]
-			}
-			if front == reverseString(back) {
-				if len(temps) > max {
-					max = len(temps)
-					word = temps
-				}
-			}
+	for i := 1; i < n-1; i++ {
+		mirror := 2*C - i
+		if R > i {
+			P[i] = min(R-i, P[mirror])
+		} else {
+			P[i] = 0
+		}
+
+		for T[i+1+P[i]] == T[i-1-P[i]] {
+			P[i]++
+		}
+
+		if i+P[i] > R {
+			C = i
+			R = i + P[i]
 		}
 	}
 
-	return word
+	maxLen, centerIndex := 0, 0
+	for i := 1; i < n-1; i++ {
+		if P[i] > maxLen {
+			maxLen = P[i]
+			centerIndex = i
+		}
+	}
+
+	start := (centerIndex - 1 - maxLen) / 2
+	return s[start : start+maxLen]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
